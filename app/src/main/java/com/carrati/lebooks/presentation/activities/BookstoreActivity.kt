08d00b81921d5ae0be487522fc0543ad3773a0f9
JuuhC <julianacarrati@hotmail.com>
2020-02-21 21:46:42 -0300

    readded alert dialog on click buy book and removed old piece of unused code

--- app/src/main/java/com/carrati/lebooks/presentation/activities/BookstoreActivity.kt
+++ app/src/main/java/com/carrati/lebooks/presentation/activities/BookstoreActivity.kt
@@ -76,23 +76,39 @@ class BookstoreActivity : AppCompatActivity(), IRecyclerViewClickListener {
     }
 
     override fun onClickBuyBook(book: StoreBook): Boolean {
-        viewModel.buyStoreBook(book)
-        var result: Boolean = false
+        var result: Boolean = false  //possivelmente esta variavel nao tera seu valor alterado :)
 
-        viewModel.stateBuyStoreBook.observe(this, Observer { state ->
-            when(state) {
-                is ViewState.Success -> {
-                    Toast.makeText(this@BookstoreActivity, "Sucesso ao comprar livro", Toast.LENGTH_LONG).show()
-                    result = true
+        val bookThumb = ImageView(this@BookstoreActivity)
+        Picasso.get().load(book.thumbURL).into(bookThumb)
+
+        val builder = AlertDialog.Builder(this@BookstoreActivity)
+        builder.setTitle("Deseja comprar este livro?").setMessage(book.title
+                + "\n" + book.writer + "\n\n")
+        builder.setView(bookThumb)
+
+        builder.setPositiveButton("OK") { _, _ ->
+
+            viewModel.buyStoreBook(book)
+
+            viewModel.stateBuyStoreBook.observe(this, Observer { state ->
+                when(state) {
+                    is ViewState.Success -> {
+                        Toast.makeText(this@BookstoreActivity, "Compra efetuada!", Toast.LENGTH_LONG).show()
+                        result = true
+                    }
+                    is ViewState.Loading ->
+                        Toast.makeText(this@BookstoreActivity, "Processando solicitação", Toast.LENGTH_SHORT).show()
+                    is ViewState.Failed -> {
+                        Toast.makeText(this@BookstoreActivity, "Erro ao comprar livro", Toast.LENGTH_LONG).show()
+                        result = false
+                    }
                 }
-                is ViewState.Loading ->
-                    Toast.makeText(this@BookstoreActivity, "Processando solicitação", Toast.LENGTH_SHORT).show()
-                is ViewState.Failed -> {
-                    Toast.makeText(this@BookstoreActivity, "Erro ao comprar livro", Toast.LENGTH_LONG).show()
-                    result = false
-                }
-            }
-        })
+            })
+        }
+        builder.setNegativeButton("Cancelar") { _, _ -> }
+
+        builder.create().show()
+
         return result
     }
 
@@ -133,114 +149,4 @@ class BookstoreActivity : AppCompatActivity(), IRecyclerViewClickListener {
 
         return super.onOptionsItemSelected(item)
     }
-
-    /*
-    1- faz o download do json e joga pra dentro de um JSONArray
-    2- percorre pegando os dados e criando objetos book
-    3- joga os objetos dentro da lista usada pelo recyclerView
-    4- notifica o adapter para recarregar a lista
-     */
-    /*private inner class JsonTask : AsyncTask<String, String, String>() {
-        internal var pd: ProgressBar? = null
-
-        override fun onPreExecute() { //exibe mensagem de carregando
-            super.onPreExecute()
-
-            pd = findViewById(R.id.bookstoreProgressBar)
-            pd?.visibility = ProgressBar.VISIBLE
-        }
-
-        override fun doInBackground(vararg params: String): String? { //solicita o download do json
-
-            var connection: HttpURLConnection? = null
-            var reader: BufferedReader? = null
-
-            try {
-                val url = URL(params[0])
-                connection = url.openConnection() as HttpURLConnection
-                connection.connect()
-
-
-                val stream = connection.inputStream
-
-                reader = BufferedReader(InputStreamReader(stream))
-
-                val buffer = StringBuilder()
-                var line: String? = null
-
-                while ({ line = reader.readLine(); line}() != null) {
-                    buffer.append(line + "\n")
-                }
-
-                return buffer.toString()
-
-
-            } catch (e: MalformedURLException) {
-                e.printStackTrace()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            } finally {
-                connection?.disconnect()
-                try {
-                    reader?.close()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-
-            }
-            return null
-        }
-
-        override fun onPostExecute(result: String) { //pega a resposta e faz o resto
-            super.onPostExecute(result)
-
-            val booksListRaw = ArrayList<Book>()
-
-            if (pd?.visibility == ProgressBar.VISIBLE) {
-                pd?.visibility = ProgressBar.GONE
-            }
-
-            if (result.isEmpty()) {
-                Toast.makeText(this@BookstoreActivity, "ERROR 1: Não foi possivel carregar a loja.", Toast.LENGTH_LONG).show()
-            } else {
-                val response: JSONArray
-                try {
-                    response = JSONArray(result)
-                } catch (e: JSONException) {
-                    Toast.makeText(this@BookstoreActivity, "ERROR 2: Não foi possivel carregar a loja.", Toast.LENGTH_LONG).show()
-                    e.printStackTrace()
-                    return
-                }
-
-                for (i in 0 until response.length()) {
-                    val obj: JSONObject
-                    val book: Book
-                    try {
-                        obj = response.getJSONObject(i)
-                        book = Book()
-                        book.title = obj.getString("title")
-                        book.price = obj.getInt("price")
-                        book.writer = obj.getString("writer")
-                        book.setThumb(obj.getString("thumbnailHd"))
-                        if(myBooksDAO?.procurarLivroFavorito(book) == true) { book.setFav() }
-                    } catch (e: Exception) {
-                        Toast.makeText(this@BookstoreActivity, "ERROR 3: Não foi possivel carregar a loja.", Toast.LENGTH_LONG).show()
-                        e.printStackTrace()
-                        return
-                    }
-
-                    //coloca na lista da loja caso não exista na lista de compras
-                    if (myBooksDAO?.procurarLivroComprado(book) == false) {
-                        //Log.i("booklist", "!procurarLivro");
-                        booksListRaw.add(book)
-                    }
-
-                }
-                booksList.addAll(booksListRaw.sortedWith(compareByDescending<Book> { it.fav }.thenBy { it.title }))
-                //Log.i("size", "booklist size = " + booksList.size());
-                adapter?.notifyDataSetChanged()
-            }
-
-        }
-    }*/
 }
